e8b3515bd4790a0291dddd918f4374445e0d4dc4
emma ingurat <e.ingurat@bsmart-technology.com>
2022-02-14 21:31:42 +0300

    added admin middleware

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -4,8 +4,16 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasFactory;
+
+    use Notifiable;
+
+    protected $table = 'admins';
+    protected $guarded = array();
+
+
+
 }
