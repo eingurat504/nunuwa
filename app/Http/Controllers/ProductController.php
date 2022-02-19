0562d877f9f638a24f6de6fd704dd8d7426f4569
emma ingurat <e.ingurat@bsmart-technology.com>
2022-02-19 23:53:10 +0300

    added product details

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,5 +6,31 @@ use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+      
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function show($id)
+    {
+
+        $product = Product::find($id);
+
+        return view('home', [
+            'product' => $product,
+        ]);
+    }
+
 }
