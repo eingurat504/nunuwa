197ad9ab1cc661eed781c38d717d45d12f81a8ae
emma ingurat <e.ingurat@bsmart-technology.com>
2022-02-19 18:29:30 +0300

    product image model modified

--- app/Models/ProductImage.php
+++ app/Models/ProductImage.php
@@ -8,4 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class ProductImage extends Model
 {
     use HasFactory;
+
+    protected $table = 'product_images';
+
+
+    protected $fillable = ['product_id', 'image_path', 'image_name'];
+
+        /**
+     * Product for this image.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function product()
+    {
+        return $this->belongsTo(Product::class,'product_id');
+    }
 }
