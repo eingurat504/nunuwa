0519475a026c8152213878e26ebc78ab7ea964ad
emma ingurat <e.ingurat@bsmart-technology.com>
2022-03-01 07:54:05 +0300

    product review model relationships

--- app/Models/ProductReview.php
+++ app/Models/ProductReview.php
@@ -4,8 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Product;
 
 class ProductReview extends Model
 {
     use HasFactory;
+
+
+     /**
+     * Product for this review.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function review()
+    {
+        return $this->belongsTo(Product::class,'product_id');
+    }
+
 }
