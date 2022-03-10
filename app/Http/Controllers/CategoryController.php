a26524edf5f00427703d2182cb7f7b25a93dbde1
emma ingurat <e.ingurat@bsmart-technology.com>
2022-03-10 15:15:09 +0300

    electronic product show

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -44,7 +44,7 @@ class CategoryController extends Controller
 
         $electronics = Product::with('category')->inRandomOrder()->take(9)->get();
 
-        return view('category.electronics', [
+        return view('category.electronics.index', [
             'electronics' => $electronics,
         ]);
     }
