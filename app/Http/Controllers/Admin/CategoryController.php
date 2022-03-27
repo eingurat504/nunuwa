2e24bf2d3684d9204ad4b82d40bba37a87239241
emma ingurat <e.ingurat@bsmart-technology.com>
2022-03-27 17:58:46 +0300

    added category methods

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -36,4 +36,82 @@ class CategoryController extends Controller
             'categories' => $categories,
         ]);
     }
+
+
+    /**
+     * Show product categories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function show($id)
+    {
+
+        $category = ProductCategory::findOrfail($id);
+
+        return view('admin.products.category.show', [
+            'category' => $category,
+        ]);
+    }
+
+
+    /**
+     * Show product categories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function edit($id)
+    {
+
+        $category = ProductCategory::findOrfail($id);
+
+        return view('admin.products.category.edit', [
+            'category' => $category,
+        ]);
+    }
+
+
+
+    /**
+     * Show product categories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function update(Request $request, $id)
+    {
+
+        $category = ProductCategory::findOrfail($id);
+
+        return view('admin.products.category.index');
+    }
+
+
+    /**
+     * Show product categories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function create()
+    {
+
+        $categories = ProductCategory::get();
+
+        return view('admin.products.category.create', [
+            'categories' => $categories,
+        ]);
+    }
+
+
+    /**
+     * Show product categories.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function store(Request $request)
+    {
+
+        $category = ProductCategory::findOrfail($id);
+
+        return view('admin.products.category.index');
+    }
+
 }
