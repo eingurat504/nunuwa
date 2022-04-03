b487b75d097dd00e19cc19c228ecff6b25b7754c
emma ingurat <e.ingurat@bsmart-technology.com>
2022-04-03 22:11:21 +0300

    added images to category details

--- resources/views/admin/products/category/show.blade.php
+++ resources/views/admin/products/category/show.blade.php
@@ -349,8 +349,9 @@
             <div class="container-xxl flex-grow-1 container-p-y">
               <h4 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"><a href="{{ route('category.index') }}">Categories</a> /</span>{{$category->name }}</h4>
 
-              <!-- Striped Rows -->
-             <div class="col-md-6">
+
+              <div class="row">
+                               <div class="col-md-6">
                 <div class="card">
                 <h5 class="card-header">Category</h5>
                
@@ -368,20 +369,104 @@
                   <label class="col-md-8">{{$category->updated_at }}</label>
                 </div>
                 </div>
-              </div>
-
-              <div class="mt-2">
+                              <div class="mt-2">
                 <button type="submit" class="btn btn-primary me-2">Edit</button>
                 <a href="{{ route('category.index') }}" class="btn btn-outline-secondary">Cancel</a>
               </div>
+              </div>
 
-             </div>
+               <div class="col-md-6">
+                  <div class="card">
+                    <h5 class="card-header">Attachments</h5>
+                    <div class="card-body">
+                       <div class="row">
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                          <div class="col-sm-6 col-lg-2 col-md-4 col-6 mb-4">
+                            <div class="card">
+                              <img class="card-img-top" src="{{ asset('admin/assets/img/elements/4.jpg') }}" alt="Card image cap">
+                            </div>
+                          </div>
+
+                      </div>
+                    </div>
+                  </div>
+              </div>
+
+              </div>
+
+              <!-- Striped Rows -->
+
+            </div>
 
             <div class="content-backdrop fade"></div>
           </div>
-          <!-- Content wrapper -->
 
-            <div class="content-backdrop fade"></div>
           </div>
           <!-- Content wrapper -->
         </div>
