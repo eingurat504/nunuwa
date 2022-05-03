77ce4734fc724cb50d477a3b6b34e1b1a81b800b
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-03 22:55:14 +0300

    added admin auth in admin module

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -19,7 +19,7 @@ class CategoryController extends Controller
     public function __construct()
     {
 
-         // $this->middleware('auth:admin');
+         $this->middleware('auth:admin');
       
     }
 
