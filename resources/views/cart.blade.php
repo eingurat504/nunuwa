294eb3682bae6cd91b527f60f8dbf7478f7270bc
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-25 23:12:02 +0300

    fixed cart layout

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -1,12 +1,12 @@
 @extends('layouts.app')
 
 @section('content')
-<!-- <div class="col-md-10">
-    <div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
+<div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
     <div class="container">
-        <h1 class="page-title">Shopping Cart<span>Shop</span></h1>
+        <h1 class="page-title">Shopping<span>Cart</span></h1>
     </div>
-</div> -->
+</div>
+
 <nav aria-label="breadcrumb" class="breadcrumb-nav">
     <div class="container">
         <ol class="breadcrumb">
@@ -14,54 +14,8 @@
             <li class="breadcrumb-item"><a href="#">Shop</a></li>
             <li class="breadcrumb-item active" aria-current="page">Shopping Cart</li>
         </ol>
-    </div><!-- End .container -->
-</nav><!-- End .breadcrumb-nav -->
-<div class="container">
-    <div class="row">
-        <div class="col-sm-6 col-lg-3">
-            <div class="banner banner-overlay">
-                <a href="#">
-                    <img src="{{ asset('images/demos/demo-13/banners/banner-1.jpg') }}" alt="Banner">
-                </a>
-
-                <div class="banner-content">
-                    <h4 class="banner-subtitle text-white"><a href="#">Weekend Sale</a></h4><!-- End .banner-subtitle -->
-                    <h3 class="banner-title text-white"><a href="#">Lighting <br>& Accessories <br><span>25% off</span></a></h3><!-- End .banner-title -->
-                    <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a>
-                </div><!-- End .banner-content -->
-            </div><!-- End .banner -->
-        </div><!-- End .col-lg-3 -->
-
-        <div class="col-sm-6 col-lg-3 order-lg-last">
-            <div class="banner banner-overlay">
-                <a href="#">
-                    <img src="{{ asset('images/demos/demo-13/banners/banner-3.jpg') }}" alt="Banner">
-                </a>
-
-                <div class="banner-content">
-                    <h4 class="banner-subtitle text-white"><a href="#">Smart Offer</a></h4><!-- End .banner-subtitle -->
-                    <h3 class="banner-title text-white"><a href="#">Anniversary <br>Special <br><span>15% off</span></a></h3><!-- End .banner-title -->
-                    <a href="#" class="banner-link">Shop Now <i class="icon-long-arrow-right"></i></a>
-                </div><!-- End .banner-content -->
-            </div><!-- End .banner -->
-        </div><!-- End .col-lg-3 -->
-
-        <div class="col-lg-6">
-            <div class="banner banner-overlay">
-                <a href="#">
-                    <img src="{{ asset('images/demos/demo-13/banners/banner-2.jpg') }}" alt="Banner">
-                </a>
-
-                <div class="banner-content">
-                    <h4 class="banner-subtitle text-white d-none d-sm-block"><a href="#">Amazing Value</a></h4><!-- End .banner-subtitle -->
-                    <h3 class="banner-title text-white"><a href="#">Clothes Trending <br>Spring Collection 2019 <br><span>from $12,99</span></a></h3><!-- End .banner-title -->
-                    <a href="#" class="banner-link">Discover Now <i class="icon-long-arrow-right"></i></a>
-                </div><!-- End .banner-content -->
-            </div><!-- End .banner -->
-        </div><!-- End .col-lg-6 -->
-    </div><!-- End .row -->
-</div><!-- End .container -->
-
+    </div>
+</nav>
 
 <div class="page-content">
     <div class="cart">
