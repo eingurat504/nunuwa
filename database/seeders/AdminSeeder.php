c996af3e7e814c61c045a94ce551d6f8725ac5e0
emma ingurat <e.ingurat@bsmart-technology.com>
2022-05-26 16:47:58 +0300

    admin seeder added

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Admin;
+
+class AdminSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+         $admin = new Admin();
+         $admin->name = 'emmanuel ituka';
+         $admin->email = 'eingurat@gmail.com';
+         $admin->password = 'ingurat504';
+         $admin->save();
+
+    }
+}
