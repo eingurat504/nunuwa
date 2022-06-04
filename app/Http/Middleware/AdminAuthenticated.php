b30ef1000eb1503eea5d12188bfe9a0ac151f8b8
emma ingurat <e.ingurat@bsmart-technology.com>
2022-06-04 22:01:14 +0300

    admin authentucated

--- app/Http/Middleware/AdminAuthenticated.php
+++ app/Http/Middleware/AdminAuthenticated.php
@@ -17,14 +17,18 @@ class AdminAuthenticated
     public function handle(Request $request, Closure $next, $guard = null)
     {
   
-        $guards = empty($guards) ? [null] : $guards;
-
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
-            }
-        }
-
+        switch ($guard) {
+            case 'admin':
+              if (Auth::guard($guard)->check()) {
+                return redirect()->route('admin.dashboard');
+              }
+             
+            default:
+              if (Auth::guard($guard)->check()) {
+                  return redirect('/');
+              }
+              break;
+          }
 
           $response = $next($request);
 
