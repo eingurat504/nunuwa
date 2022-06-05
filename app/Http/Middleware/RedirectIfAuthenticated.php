92136b65a198366ae53da1d58911507dae3cfb7f
emma ingurat <e.ingurat@bsmart-technology.com>
2022-06-05 21:37:17 +0300

    middleware fixes

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,13 +19,6 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, $guard = null)
     {
-        // $guards = empty($guards) ? [null] : $guards;
-
-        // foreach ($guards as $guard) {
-        //     if (Auth::guard($guard)->check()) {
-        //         return redirect(RouteServiceProvider::HOME);
-        //     }
-        // }
 
         switch ($guard) {
             case 'admin':
