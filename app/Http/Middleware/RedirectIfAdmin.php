90af57f90a6695afb307d595058e545ab0163cf1
emma ingurat <e.ingurat@bsmart-technology.com>
2022-06-21 18:30:16 +0300

    redirect if admin

---
+++ app/Http/Middleware/RedirectIfAdmin.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class RedirectIfAdmin
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    public function handle(Request $request, Closure $next, $guard = 'admin')
+    {
+  
+      if (Auth::guard($guard)->check()) {
+        return redirect()->route('admin.home');
+      }
+
+    return $next($request);
+    
+    }
+}
