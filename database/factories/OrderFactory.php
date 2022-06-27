fc01c0cb38de7b318842488d4d8f1ee47081bc38
emma ingurat <e.ingurat@bsmart-technology.com>
2022-06-27 15:02:57 +0300

    fixed orders foreignIds

--- database/factories/OrderFactory.php
+++ database/factories/OrderFactory.php
@@ -17,14 +17,19 @@ class OrderFactory extends Factory
         return [
 
             'order_date' => $this->faker->dateTimeThisMonth()->format('Y-m-d H:i:s'),
-            'email' => $this->faker->sentence(4),
-            'phone_no' => $this->faker->sentence(4),
-            'amount' => 300,
+            'billing_first_name' => $this->faker->sentence(4),
+            'billing_last_name' => $this->faker->sentence(4),
+            'billing_email' => $this->faker->sentence(4),
+            'billing_phone' => $this->faker->sentence(4),
+            'tax' => 10,
+            'sub_total' => 300,
+            'total' => 300,
             'tracking_no' => $this->faker->sentence(4),
-            'city' => $this->faker->sentence(4),
-            'country' => $this->faker->sentence(4),
-            'shipping_address' => $this->faker->sentence(4),
-            'shipping_address_2' => $this->faker->sentence(4),
+            'billing_city' => $this->faker->sentence(4),
+            'billing_country' => $this->faker->sentence(4),
+            'billing_state' => $this->faker->sentence(4),
+            'billing_address_1' => $this->faker->sentence(4),
+            'billing_address_2' => $this->faker->sentence(4),
             'user_id' => function () {
                 return User::factory()->create()->id;
             },
