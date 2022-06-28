d18c954f3333f9b2e86af236add7adf49c14e1ec
emma ingurat <e.ingurat@bsmart-technology.com>
2022-06-28 12:07:00 +0300

    cart index updates

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -34,52 +34,31 @@
                         </thead>
 
                         <tbody>
+                            @foreach (Cart::instance('default')->content() as $item)
                             <tr>
-                                <td class="product-col">
-                                    <div class="product">
-                                        <figure class="product-media">
-                                            <a href="#">
-                                                <img src="{{ asset('images/products/table/product-1.jpg') }}" alt="Product image">
-                                            </a>
-                                        </figure>
+                                    <td class="product-col">
+                                        <div class="product">
+                                            <figure class="product-media">
+                                                <a href="#">
+                                                    <img src="{{ asset('images/products/table/product-1.jpg') }}" alt="Product image">
+                                                </a>
+                                            </figure>
 
-                                        <h3 class="product-title">
-                                            <a href="#">Beige knitted elastic runner shoes</a>
-                                        </h3><!-- End .product-title -->
-                                    </div><!-- End .product -->
-                                </td>
-                                <td class="price-col">$84.00</td>
-                                <td class="quantity-col">
-                                    <div class="cart-product-quantity">
-                                        <input type="number" class="form-control" value="1" min="1" max="10" step="1" data-decimals="0" required>
-                                    </div><!-- End .cart-product-quantity -->
-                                </td>
-                                <td class="total-col">$84.00</td>
-                                <td class="remove-col"><button class="btn-remove"><i class="icon-close"></i></button></td>
-                            </tr>
-                            <tr>
-                                <td class="product-col">
-                                    <div class="product">
-                                        <figure class="product-media">
-                                            <a href="#">
-                                                <img src="{{ asset('images/products/table/product-2.jpg') }}" alt="Product image">
-                                            </a>
-                                        </figure>
-
-                                        <h3 class="product-title">
-                                            <a href="#">Blue utility pinafore denim dress</a>
-                                        </h3><!-- End .product-title -->
-                                    </div><!-- End .product -->
-                                </td>
-                                <td class="price-col">$76.00</td>
-                                <td class="quantity-col">
-                                    <div class="cart-product-quantity">
-                                        <input type="number" class="form-control" value="1" min="1" max="10" step="1" data-decimals="0" required>
-                                    </div><!-- End .cart-product-quantity -->                                 
-                                </td>
-                                <td class="total-col">$76.00</td>
-                                <td class="remove-col"><button class="btn-remove"><i class="icon-close"></i></button></td>
-                            </tr>
+                                            <h3 class="product-title">
+                                                <a href="{{ route('products.show', $item->id) }}">{{ $item->model->name }}</a>
+                                            </h3><!-- End .product-title -->
+                                        </div><!-- End .product -->
+                                    </td>
+                                    <td class="price-col">${{ $item->subtotal() }}</td>
+                                    <td class="quantity-col">
+                                        <div class="cart-product-quantity">
+                                            <input type="number" class="form-control" value="1" min="1" max="10" step="1" data-decimals="0" required>
+                                        </div><!-- End .cart-product-quantity -->
+                                    </td>
+                                    <td class="total-col">${{ $item->subtotal() }}</td>
+                                    <td class="remove-col"><button class="btn-remove"><i class="icon-close"></i></button></td>
+                                </tr>
+                            @endforeach
                         </tbody>
                     </table><!-- End .table table-wishlist -->
 
