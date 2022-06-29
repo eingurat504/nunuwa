f3bc5a68b86ca1f123de6341b9e84d32b06840d6
emma ingurat <e.ingurat@bsmart-technology.com>
2022-06-29 21:31:54 +0300

    audio and travel cart button fixes

--- resources/views/category/tv_audio.blade.php
+++ resources/views/category/tv_audio.blade.php
@@ -101,9 +101,15 @@
                                             <a href="#" class="btn-product-icon btn-compare" title="Compare"><span>Compare</span></a>
                                         </div>
 
-                                        <div class="product-action">
-                                            <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
-                                        </div>
+                                        <form action="{{ route('cart.store') }}" method="POST">
+                                            {{ csrf_field() }}
+                                            <input type="hidden" name="id" value="{{ $audio->id }}">
+                                            <input type="hidden" name="name" value="{{ $audio->name }}">
+                                            <input type="hidden" name="price" value="{{ $audio->price }}">
+                                            <div class="product-action">
+                                                <button type="submit" class="btn-product btn-cart">add to cart</button>
+                                            </div>
+                                        </form>
                                     </figure>
 
                                     <div class="product-body">
