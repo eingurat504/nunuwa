788a10cd65d169dff66627869fccb44cde22466f
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-03 21:21:41 +0300

    smart phoes cart button

--- resources/views/category/phones.blade.php
+++ resources/views/category/phones.blade.php
@@ -102,9 +102,15 @@
                                             <a href="#" class="btn-product-icon btn-compare" title="Compare"><span>Compare</span></a>
                                         </div>
 
-                                        <div class="product-action">
-                                            <a href="#" class="btn-product btn-cart"><span>add to cart</span></a>
-                                        </div>
+                                        <form action="{{ route('cart.store') }}" method="POST">
+                                            {{ csrf_field() }}
+                                            <input type="hidden" name="id" value="{{ $phone->id }}">
+                                            <input type="hidden" name="name" value="{{ $phone->name }}">
+                                            <input type="hidden" name="price" value="{{ $phone->price }}">
+                                            <div class="product-action">
+                                                <button type="submit" class="btn-product btn-cart">add to cart</button>
+                                            </div>
+                                        </form>
                                     </figure>
 
                                     <div class="product-body">
