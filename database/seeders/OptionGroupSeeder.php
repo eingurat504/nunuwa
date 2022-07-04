a0e5a8e8da893ee266b68bf9979eda53cd1f074a
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-04 12:18:58 +0300

    optionseeder fixes

---
+++ database/seeders/OptionGroupSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\OptionGroup;
+
+class OptionGroupSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //  $option_group = OptionGroup::firstOrCreate(
+        //      ['name'=>'Color'],
+        //      ['name'=>'Size']
+        // );
+        //  $option_group->save();
+
+    }
+}
--- database/seeders/OptionSeeder.php
+++ database/seeders/OptionSeeder.php
@@ -14,17 +14,18 @@ class OptionSeeder extends Seeder
      */
     public function run()
     {
-         $option = Option::firstOrCreate(
-             ['name'=>'red','option_group_id'=>1],
-             ['name'=>'blue','option_group_id'=>1],
-             ['name'=>'green','option_group_id'=>1],
-             ['name'=>'S','option_group_id'=>2],
-             ['name'=>'M','option_group_id'=>2],
-             ['name'=>'L','option_group_id'=>2],
-             ['name'=>'XL','option_group_id'=>2],
-             ['name'=>'XXL','option_group_id'=>2]
-            );
-         $option->save();
+      
+        //  $option = Option::firstOrCreate(
+        //      ['name'=>'red','option_group_id'=>1],
+        //      ['name'=>'blue','option_group_id'=>1],
+        //      ['name'=>'green','option_group_id'=>1],
+        //      ['name'=>'S','option_group_id'=>2],
+        //      ['name'=>'M','option_group_id'=>2],
+        //      ['name'=>'L','option_group_id'=>2],
+        //      ['name'=>'XL','option_group_id'=>2],
+        //      ['name'=>'XXL','option_group_id'=>2]
+        //     );
+        //  $option->save();
 
     }
 }
