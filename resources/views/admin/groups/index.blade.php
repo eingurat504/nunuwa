91b82b1ca12c72f29bf1f1f1c7d43da02c6bd46a
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-06 21:13:29 +0300

    datables in group

--- resources/views/admin/groups/index.blade.php
+++ resources/views/admin/groups/index.blade.php
@@ -24,7 +24,7 @@
     </div>
     <div class="card-body">
         <div class="table-responsive text-nowrap">
-            <table id="tbl_option_groups" class="table table-striped">
+            <table id="tbl_products" class="table table-striped">
               <thead>
                 <tr>
                   <th>Name</th>
