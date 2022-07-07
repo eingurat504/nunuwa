cee346f7f8dbe379fd833d61762c8059d6ca402b
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-07 16:09:53 +0300

    option groups migration updates '

--- database/migrations/2022_02_09_125105_create_option_groups_table.php
+++ database/migrations/2022_02_09_125105_create_option_groups_table.php
@@ -16,6 +16,7 @@ class CreateOptionGroupsTable extends Migration
         Schema::create('option_groups', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
+            $table->string('description');
             $table->timestamps();
         });
     }
