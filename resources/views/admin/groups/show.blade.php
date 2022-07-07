dd317698edd9bfa954b9d19688616513086cf6ff
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-07 16:03:22 +0300

    groups show fixes

--- resources/views/admin/groups/show.blade.php
+++ resources/views/admin/groups/show.blade.php
@@ -2,23 +2,18 @@
 
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
-    <h6 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"><a href="{{ route('admin.option_groups.index') }}">Option Groups</a> /</span>{{$group->name }}</h6>
+    <h6 class="fw-bold py-3 mb-4"><span class="text-muted fw-light"><a href="{{ route('admin.dashboard') }}">Home</a> / <a href="{{ route('admin.option_groups.index') }}">Option Groups</a> /</span>{{$group->name }}</h6>
 
     <div class="row">
       <div class="col-md-6">
 
         <div class="card">
-          <h5 class="card-header">Otion Group</h5>
+          <h5 class="card-header">Option Group</h5>
 
             <div class="card-body">
               <div class="row">
                 <label class="col-md-4">Name</label>
                 <label class="col-md-4">{{ $group->name }}</label>
-              </div>
-
-              <div class="row">
-                <label class="col-md-4">Price</label>
-                <label class="col-md-4">${{$group->price }}</label>
               </div>
 
               <div class="row">
