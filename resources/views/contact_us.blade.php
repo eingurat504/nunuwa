51096422fb908e369c135ce3cc6cbe78446a282b
emma ingurat <e.ingurat@bsmart-technology.com>
2022-07-22 23:10:42 +0300

    form validation

--- resources/views/contact_us.blade.php
+++ resources/views/contact_us.blade.php
@@ -4,7 +4,7 @@
 <div class="page-header text-center" style="background-image: url('assets/images/page-header-bg.jpg')">
     <div class="container">
         <h1 class="page-title">Contact Us<span>Pages</span></h1>
-    </div><!-- End .container -->
+    </div>
 </div><!-- End .page-header -->
 <nav aria-label="breadcrumb" class="breadcrumb-nav border-0 mb-0">
     <div class="container">
@@ -13,7 +13,7 @@
             <li class="breadcrumb-item"><a href="#">Pages</a></li>
             <li class="breadcrumb-item active" aria-current="page">Contact us 2</li>
         </ol>
-    </div><!-- End .container -->
+    </div>
 </nav><!-- End .breadcrumb-nav -->
 
 <div class="page-content">
@@ -50,7 +50,7 @@
                     </div><!-- End .soial-icons -->
                 </div><!-- End .contact-box -->
             </div><!-- End .col-md-4 -->
-        </div><!-- End .row -->
+        </div>
 
         <hr class="mt-3 mb-5 mt-md-1">
         <div class="touch-container row justify-content-center">
@@ -61,41 +61,70 @@
                     We collaborate with ambitious brands and people; we’d love to build something great together.
                 </p><!-- End .lead text-primary -->
                 <p class="mb-3">Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.</p>
-                </div><!-- End .text-center -->
+                </div>
 
                 <form action="#" class="contact-form mb-2">
                     <div class="row">
                         <div class="col-sm-4">
-                            <label for="cname" class="sr-only">Name</label>
-                            <input type="text" class="form-control" id="cname" placeholder="Name *" required>
+                            <label for="name" class="sr-only">Name</label>
+                            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Name *" required>
+                            @error('name')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror  
                         </div><!-- End .col-sm-4 -->
 
                         <div class="col-sm-4">
-                            <label for="cemail" class="sr-only">Name</label>
-                            <input type="email" class="form-control" id="cemail" placeholder="Email *" required>
+                            <label for="email" class="sr-only">Email</label>
+                            <input type="email" class="form-control @error('email') is-invalid @enderror" name="email" id="email" placeholder="Email *" required>
+                            @error('email')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror  
                         </div><!-- End .col-sm-4 -->
 
                         <div class="col-sm-4">
-                            <label for="cphone" class="sr-only">Phone</label>
-                            <input type="tel" class="form-control" id="cphone" placeholder="Phone">
+                            <label for="phone" class="sr-only">Phone</label>
+                            <input type="tel" name="phone" class="form-control @error('phone') is-invalid @enderror" id="phone" placeholder="Phone">
+                            @error('phone')
+                                <span class="invalid-feedback" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
+                            @enderror  
                         </div><!-- End .col-sm-4 -->
-                    </div><!-- End .row -->
+                    </div>
 
-                    <label for="csubject" class="sr-only">Subject</label>
-                    <input type="text" class="form-control" id="csubject" placeholder="Subject">
+                    <div class="row">
+                        <label for="subject" class="sr-only">Subject</label>
+                        <input type="text" name="subject" class="form-control @error('subject') is-invalid @enderror" id="subject" placeholder="Subject">
+                        @error('subject')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror  
+                    </div>
 
-                    <label for="cmessage" class="sr-only">Message</label>
-                    <textarea class="form-control" cols="30" rows="4" id="cmessage" required placeholder="Message *"></textarea>
-                    
+                    <div class="row">
+                        <label for="message" class="sr-only">Message</label>
+                        <textarea class="form-control @error('message') is-invalid @enderror" name="message" cols="30" rows="4" id="message" required placeholder="Message *"></textarea>
+                        @error('message')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror  
+                    </div>
+
                     <div class="text-center">
                         <button type="submit" class="btn btn-outline-primary-2 btn-minwidth-sm">
                             <span>SUBMIT</span>
                             <i class="icon-long-arrow-right"></i>
                         </button>
-                    </div><!-- End .text-center -->
-                </form><!-- End .contact-form -->
-            </div><!-- End .col-md-9 col-lg-7 -->
-        </div><!-- End .row -->
-    </div><!-- End .container -->
-</div><!-- End .page-content -->
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
