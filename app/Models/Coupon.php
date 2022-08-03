62784f70de4eedf6001023d88c1dde590a8f5181
emma ingurat <e.ingurat@bsmart-technology.com>
2022-08-03 21:29:58 +0300

    coupon model added

---
+++ app/Models/Coupon.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Coupon extends Model
+{
+    use HasFactory;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'options';
+
+
+    /**
+     * The primary key for the model.
+     *
+     * @var string
+     */
+    protected $primaryKey = 'id';
+
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'code',
+        'type',
+        'value',
+        'percent_off',
+    ];
+
+
+
+
+
+}
