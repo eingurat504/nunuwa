8aacc1c7beff08a23b8037490e65eccc569d4964
emma ingurat <e.ingurat@bsmart-technology.com>
2022-08-07 20:29:57 +0300

    coupon model updates

--- app/Models/Coupon.php
+++ app/Models/Coupon.php
@@ -37,4 +37,22 @@ class Coupon extends Model
         'percent_off',
     ];
 
+
+    public function findByCode($code){
+        return self::where('code', $code)->first();
+     }
+ 
+     /**
+      * discount function
+      */
+     public function discount($total){
+         if($this->type == 'fixed'){
+             return $this->value;
+         } elseif($this->type == 'percent'){
+             return ($this->percent_off / 100) * $total;
+         } else {
+             return 0;
+         }
+     }
+
 }
