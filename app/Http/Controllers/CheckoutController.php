b7a3a4fbf9000423217120c5828ab65b044ca54c
emma ingurat <e.ingurat@bsmart-technology.com>
2022-09-02 23:59:30 +0300

    added helper class

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Support\Helper;
+use Gloudemans\Shoppingcart\Facades\Cart;
 use Illuminate\Support\Facades\Auth;
 
 class CheckoutController extends Controller
