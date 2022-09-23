4d286675d6c7d914d58b0c0856b74ebb72690cb7
emma ingurat <e.ingurat@bsmart-technology.com>
2022-09-23 23:30:59 +0300

    added wishlists blade updates

--- resources/views/wishlist.blade.php
+++ resources/views/wishlist.blade.php
@@ -23,34 +23,36 @@
                 <tr>
                     <th>Product</th>
                     <th>Price</th>
-                    <th>Stock Status</th>
+                    <!-- <th>Stock Status</th> -->
                     <th></th>
                     <th></th>
                 </tr>
             </thead>
 
             <tbody>
-                <!-- <tr>
-                    <td class="product-col">
-                        <div class="product">
-                            <figure class="product-media">
-                                <a href="#">
-                                    <img src="{{ asset('images/products/table/product-3.jpg') }}" alt="Product image">
-                                </a>
-                            </figure>
+                @foreach($wishlists as $wishlist) 
+                    <tr>
+                        <td class="product-col">
+                            <div class="product">
+                                <figure class="product-media">
+                                    <a href="#">
+                                        <img src="{{ asset('images/products/table/product-3.jpg') }}" alt="Product image">
+                                    </a>
+                                </figure>
 
-                            <h3 class="product-title">
-                                <a href="#">Orange saddle lock front chain cross body bag</a>
-                            </h3>
-                        </div>
-                    </td>
-                    <td class="price-col">$52.00</td>
-                    <td class="stock-col"><span class="out-of-stock">Out of stock</span></td>
-                    <td class="action-col">
-                        <button class="btn btn-block btn-outline-primary-2 disabled">Out of Stock</button>
-                    </td>
-                    <td class="remove-col"><button class="btn-remove"><i class="icon-close"></i></button></td>
-                </tr> -->
+                                <h3 class="product-title">
+                                    <a href="{{ route('products.show') }}">{{ $wishlist->product->name }}</a>
+                                </h3>
+                            </div>
+                        </td>
+                        <td class="price-col">{{ $wishlist->product->price }}</td>
+                        <!-- <td class="stock-col"><span class="out-of-stock">Out of stock</span></td> -->
+                        <!-- <td class="action-col">
+                            <button class="btn btn-block btn-outline-primary-2 disabled">Out of Stock</button>
+                        </td> -->
+                        <td class="remove-col"><button class="btn-remove"><i class="icon-close"></i></button></td>
+                    </tr>
+                @endforeach
             </tbody>
         </table>
         <div class="wishlist-share">
