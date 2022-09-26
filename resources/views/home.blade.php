13262c7882f351f68fa07422540660b1dc774d8e
emma ingurat <e.ingurat@bsmart-technology.com>
2022-09-26 21:00:56 +0300

    categories updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -84,77 +84,19 @@
 
     <div class="cat-blocks-container">
         <div class="row">
-            <div class="col-6 col-sm-4 col-lg-2">
-                <a href="category.html" class="cat-block">
-                    <figure>
-                        <span>
-                            <img src="{{ asset('images/demos/demo-13/cats/1.jpg') }}" alt="Category image">
-                        </span>
-                    </figure>
+            @foreach($categories as $category) 
+                <div class="col-6 col-sm-4 col-lg-2">
+                    <a href="category.html" class="cat-block">
+                        <figure>
+                            <span>
+                                <img src="{{ asset('images/demos/demo-13/cats/1.jpg') }}" alt="Category image">
+                            </span>
+                        </figure>
 
-                    <h3 class="cat-block-title">Computer & Laptop</h3>
-                </a>
-            </div>
-
-            <div class="col-6 col-sm-4 col-lg-2">
-                <a href="category.html" class="cat-block">
-                    <figure>
-                        <span>
-                            <img src="{{ asset('images/demos/demo-13/cats/2.jpg') }}" alt="Category image">
-                        </span>
-                    </figure>
-
-                    <h3 class="cat-block-title">Lighting</h3>
-                </a>
-            </div>
-
-            <div class="col-6 col-sm-4 col-lg-2">
-                <a href="category.html" class="cat-block">
-                    <figure>
-                        <span>
-                            <img src="{{ asset('images/demos/demo-13/cats/3.jpg') }}" alt="Category image">
-                        </span>
-                    </figure>
-
-                    <h3 class="cat-block-title">Smart Phones</h3>
-                </a>
-            </div>
-
-            <div class="col-6 col-sm-4 col-lg-2">
-                <a href="category.html" class="cat-block">
-                    <figure>
-                        <span>
-                            <img src="{{ asset('images/demos/demo-13/cats/4.jpg') }}" alt="Category image">
-                        </span>
-                    </figure>
-
-                    <h3 class="cat-block-title">Televisions</h3>
-                </a>
-            </div>
-
-            <div class="col-6 col-sm-4 col-lg-2">
-                <a href="category.html" class="cat-block">
-                    <figure>
-                        <span>
-                            <img src="{{ asset('images/demos/demo-13/cats/5.jpg') }}" alt="Category image">
-                        </span>
-                    </figure>
-
-                    <h3 class="cat-block-title">Cooking</h3>
-                </a>
-            </div>
-
-            <div class="col-6 col-sm-4 col-lg-2">
-                <a href="category.html" class="cat-block">
-                    <figure>
-                        <span>
-                            <img src="{{ asset('images/demos/demo-13/cats/6.jpg') }}" alt="Category image">
-                        </span>
-                    </figure>
-
-                    <h3 class="cat-block-title">Furniture</h3>
-                </a>
-            </div>
+                        <h3 class="cat-block-title">{{ $category->name }}</h3>
+                    </a>
+                </div>
+            @endforeach
         </div>
     </div>
 </div>
