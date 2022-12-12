ac407164bb3d358d3f10d5250e5754124b852cf7
emma ingurat <e.ingurat@bsmart-technology.com>
2022-12-12 21:35:00 +0100

    edit form validations

--- resources/views/admin/coupons/edit.blade.php
+++ resources/views/admin/coupons/edit.blade.php
@@ -25,26 +25,56 @@
                   <div class="mb-3 col-md-12">
                     <label for="code" class="form-label">Coupon Code</label>
                     <input class="form-control" type="text" id="code" name="code" value="{{ old('code', $coupon->code) }}"/>
+                  @error('code')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror 
                   </div>
                   <div class="mb-3 col-md-12">
                     <label for="type" class="form-label">Type</label>
                     <input class="form-control" type="text" id="type" name="type" value="{{ old('type',  $coupon->type) }}"/>
+                  @error('type')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror 
                   </div>
                   <div class="mb-3 col-md-12">
                     <label for="value" class="form-label">Value</label>
                     <input class="form-control" type="text" id="value" name="value" value="{{ old('value',  $coupon->value) }}"/>
+                  @error('value')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror 
                   </div>
                   <div class="mb-3 col-md-12">
                     <label for="percent_off" class="form-label">Percent Off</label>
                     <input class="form-control" type="text" id="percent_off" name="percent_off" value="{{ old('percent_off',  $coupon->percent_off) }}"/>
+                  @error('percent_off')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror 
                   </div>
                   <div class="mb-3 col-md-12">
                     <label for="expires_at" class="form-label">Expiry Date</label>
                     <input class="form-control" type="date" id="expires_at" name="expires_at" value="{{ old('expires_at', $coupon->expires_at) }}"/>
+                  @error('expires_at')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror 
                   </div>
                   <div class="mb-3 col-md-12">
                     <label for="usable_times" class="form-label">Usable times</label>
                     <input class="form-control" type="number" id="usable_times" name="usable_times" value="{{ old('usable_times', $coupon->usable_times) }}"/>
+                  @error('usable_times')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                  @enderror 
                   </div>
                   <div class="mt-2">
                     <button type="submit" class="btn btn-primary me-2">Save</button>
