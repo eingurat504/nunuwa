d6c43d7e2d8a7f8f4e9d698a257d12dc71e25457
emma ingurat <e.ingurat@bsmart-technology.com>
2023-03-03 22:27:21 +0300

    auth updates

--- app/Http/Controllers/Admin/Auth/AuthController.php
+++ app/Http/Controllers/Admin/Auth/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
      * @return \App\Models\User
      */
     protected function getlogin()
-    {
+    {  
 
         return view('admin.auth.login');
     }
