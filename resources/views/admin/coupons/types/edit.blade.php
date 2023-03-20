1cd55db362c28a40186612fa27df99d73bcad138
emma ingurat <e.ingurat@bsmart-technology.com>
2023-03-20 23:03:31 +0300

    added sidebar link

--- resources/views/admin/coupons/types/edit.blade.php
+++ resources/views/admin/coupons/types/edit.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
 <div class="container-xxl flex-grow-1 container-p-y">
-  <h6 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Tables /</span> Coupon Types/ {{ $type->name }} /Update</h6>
+  <h6 class="fw-bold py-3 mb-4"><span class="text-muted fw-light">Home / </span><span class="text-muted fw-light"><a href="{{ route('admin.coupon_types.index') }}">Coupon Types </a> /</span>{{ $type->name }} /Update</h6>
 
   <div class="row">
       <div class="col-md-12">
@@ -12,7 +12,7 @@
   <div class="col-md-6">
   <div class="card">
     <div class="card-header d-flex align-items-center justify-content-between">
-      <h5 class="card-title m-0 me-2">Coupon Codes</h5>
+      <h5 class="card-title m-0 me-2">Coupon Types</h5>
     </div>
     <div class="card-body">
         <form method="POST" action="{{ route('admin.coupon_types.update', $type->id) }}">
